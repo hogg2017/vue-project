8faea1e5a62e0ef0fad8b18dbe30505ddf2b68ec
hi <deuntlt@vip.qq.com>
2021-01-19 16:58:29 +0800

    depen

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
+import { Button } from 'element-ui'
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./plugins/element.js";
 
 Vue.config.productionTip = false;
+Vue.use(Button)
 
 new Vue({
   router,
