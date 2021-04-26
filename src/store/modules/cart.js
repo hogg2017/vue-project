b65a814f325d2ef052c3dcd6aa72edeaac9d4b6d
hi <deuntlt@vip.qq.com>
2021-04-26 17:32:06 +0800

    build a  lazy component

--- src/store/modules/cart.js
+++ src/store/modules/cart.js
@@ -1,96 +1,22 @@
-//使用module将mutation分块
-//改变数据--> 病毒混合之后效果是不一样的,导致不一样的感染个体
-import shop from '../../utils/shopRequest'
-import { CART, PRODUCTS } from '../mutation-types'
+//购物车
 
+//vuex 中 state 模块,定义当前功能用到的变量数据
+//页面调用:this.$store.state.cart
 const state = {
-  item: [],
-  checkoutStatus: null
-}
+  cartList:[],//购物车数据
+  allSelected:false,//购物车的全选状态
+  cartNum: '', //购物车,涉及到刷新数据丢失
 
-const getters = {
-  cartProducts: (state, getters, rootState) => {
-    return state.items.map(({ id, quantity }) => {
-      const product = rootState.products.all.find(product => {
-        product.id === id
-      })
-      return {
-        title: product.title,
-        price: product.price,
-        quantity
-      }
-    })
-  },
-  cartTotalPrice: (state, getters) => {
-    return getters.cartProducts.reduce((total, product) => {
-      return total + product.price * product.quantity
-    }, 0)
+}
+//vuex 中的actions模块,主要定义一些异步方法
+//页面触发调用 this.$store.dispatch(‘方法名’,参数只能有一个)
+//action 和 mutation 都可以改变state中的数据的状态,但是action 可以处理异步函数
+const action = {
+  //购物车数据(查)
+  getCartList({
+    commit,
+    state
+  }){
+    
   }
-}
-// actions
-const actions = {
-  checkout({ commit, state }, products) {
-    const savedCartItems = [...state.items]
-    commit(CART.SET_CHECKOUT_STATUS, null)
-    // empty cart
-    commit(CART.SET_CART_ITEMS, { items: [] })
-    shop.buyProducts(
-      products,
-      () => commit(CART.SET_CHECKOUT_STATUS, 'successful'),
-      () => {
-        commit(CART.SET_CHECKOUT_STATUS, 'failed')
-        // rollback to the cart saved before sending the request
-        commit(CART.SET_CART_ITEMS, { items: savedCartItems })
-      }
-    )
-  },
-
-  addProductToCart({ state, commit }, { product, number }) {
-    commit(CART.SET_CHECKOUT_STATUS, null)
-    if (product.inventory > 0) {
-      const cartItem = state.items.find(item => item.id === product.id)
-      if (!cartItem) {
-        commit(CART.PUSH_PRODUCT_TO_CART, { id: product.id, number })
-      } else {
-        commit(CART.INCREMENT_ITEM_QUANTITY, { cartItem, number })
-      }
-      // remove number item from stock
-      commit(
-        `products/${PRODUCTS.DECREMENT_PRODUCT_INVENTORY}`,
-        { id: product.id, number },
-        { root: true }
-      )
-    }
-  }
-}
-
-// mutations
-const mutations = {
-  [CART.PUSH_PRODUCT_TO_CART](state, { id, number }) {
-    state.items.push({
-      id,
-      quantity: number
-    })
-  },
-
-  [CART.INCREMENT_ITEM_QUANTITY](state, { cartItem: { id }, number }) {
-    const cartItem = state.items.find(item => item.id === id)
-    cartItem.quantity += number
-  },
-
-  [CART.SET_CART_ITEMS](state, { items }) {
-    state.items = items
-  },
-
-  [CART.SET_CHECKOUT_STATUS](state, status) {
-    state.checkoutStatus = status
-  }
-}
-
-export default {
-  namespaced: true,
-  state,
-  getters,
-  actions,
-  mutations
 }
